aa6c1264c72d52042e4c83dccfd86e40dffc2e85
greg <xurxo654@gmail.com>
2025-08-15 12:32:13 -0400

    basic statistics completed.

--- src/test/kotlin/StatisticsTests.kt
+++ src/test/kotlin/StatisticsTests.kt
@@ -74,4 +74,58 @@ class StatisticsTests {
         )
         assertEquals(expected, calcDispersion(shots))
     }
+
+    @Test
+    fun `calcClubDispersion should return map`() {
+        val shots = listOf(Shot(Club.DR, ShotResult.LEFT), Shot(Club.DR, ShotResult.RIGHT), Shot(Club.DR, ShotResult.ON_TARGET))
+
+        val expected = mapOf(
+            ShotResult.LONG to 0.0,
+            ShotResult.LONG_LEFT to 0.0,
+            ShotResult.LONG_RIGHT to 0.0,
+            ShotResult.LEFT to 0.0,
+            ShotResult.RIGHT to 0.0,
+            ShotResult.ON_TARGET to 0.0,
+            ShotResult.SHORT to 0.0,
+            ShotResult.SHORT_LEFT to 0.0,
+            ShotResult.SHORT_RIGHT to 0.0,
+        )
+        assertEquals(expected, calcClubDispersion(shots, Club.SIX_IRON))
+    }
+
+    @Test
+    fun `calcClubDispersion should return zero when club has not been used`() {
+        val shots = listOf(Shot(Club.DR, ShotResult.LEFT), Shot(Club.DR, ShotResult.RIGHT), Shot(Club.DR, ShotResult.ON_TARGET))
+
+        val expected = mapOf(
+            ShotResult.LONG to 0.0,
+            ShotResult.LONG_LEFT to 0.0,
+            ShotResult.LONG_RIGHT to 0.0,
+            ShotResult.LEFT to 0.0,
+            ShotResult.RIGHT to 0.0,
+            ShotResult.ON_TARGET to 0.0,
+            ShotResult.SHORT to 0.0,
+            ShotResult.SHORT_LEFT to 0.0,
+            ShotResult.SHORT_RIGHT to 0.0,
+        )
+        assertEquals(expected, calcClubDispersion(shots, Club.SIX_IRON))
+    }
+
+    @Test
+    fun `calcClubDispersion should return map with only results for passed club`() {
+        val shots = listOf(Shot(Club.DR, ShotResult.LEFT), Shot(Club.DR, ShotResult.RIGHT), Shot(Club.NINE_IRON, ShotResult.RIGHT))
+
+        val expected = mapOf(
+            ShotResult.LONG to 0.0,
+            ShotResult.LONG_LEFT to 0.0,
+            ShotResult.LONG_RIGHT to 0.0,
+            ShotResult.LEFT to 50.0,
+            ShotResult.RIGHT to 50.0,
+            ShotResult.ON_TARGET to 0.0,
+            ShotResult.SHORT to 0.0,
+            ShotResult.SHORT_LEFT to 0.0,
+            ShotResult.SHORT_RIGHT to 0.0,
+        )
+        assertEquals(expected, calcClubDispersion(shots, Club.DR))
+    }
 }
