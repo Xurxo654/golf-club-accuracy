b05db30ff4c465166fe4b290d329f6c1089b2ea0
greg <xurxo654@gmail.com>
2025-08-17 12:58:34 -0400

    addShot method added and passing all test cases.

--- src/main/kotlin/ConsoleApp.kt
+++ src/main/kotlin/ConsoleApp.kt
@@ -8,7 +8,34 @@ class ConsoleApp {
         val command = readlnOrNull() ?: return
         when (command) {
             "quit" -> println("Exiting Application")
+            "shoot" -> addShot()
             else -> System.err.println("Invalid command: $command")
         }
+    }
+
+    private fun addShot() {
+        while (true) {
+            println("Select Club:")
+            val clubInput = readlnOrNull() ?: continue
+            val club = Club.ofClubName(clubInput)
+            if (club == null) {
+                System.err.println("Invalid club: $clubInput")
+                continue
+            } else {
+                break
+            }
+        }
+        while (true) {
+            println("Select Result:")
+            val resultInput = readlnOrNull() ?: continue
+            val result = ShotResult.ofResultName(resultInput)
+            if (result == null) {
+                System.err.println("Invalid result: $resultInput")
+                continue
+            } else {
+                break
+            }
+        }
+        println("Shot added")
     }
 }
